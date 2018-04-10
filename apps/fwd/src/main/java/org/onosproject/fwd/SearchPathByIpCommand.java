9e6816303971a584c5a5c658429a0384e2133682
jiujiuwo <1203574299@qq.com>
2018-04-10 17:11:56 +0800

    新建命令行类,通过命令行查找ip地址对儿的路径

---
+++ apps/fwd/src/main/java/org/onosproject/fwd/SearchPathByIpCommand.java
@@ -0,0 +1,36 @@
+package org.onosproject.fwd;
+
+/**
+ * Created by lihaifeng on 18-4-10.
+ */
+
+import org.apache.karaf.shell.commands.Command;
+import org.onosproject.cli.AbstractShellCommand;
+import org.apache.karaf.shell.commands.Argument;
+import org.onlab.packet.MacAddress;
+
+/**
+ *  根据IP地址找出路径
+ */
+@Command(scope = "onos", name = "fwd-apbi",
+        description = "根据输入的IP地址对查找路径")
+public class SearchPathByIpCommand extends AbstractShellCommand{
+
+    @Argument(index = 0, name = "ipSrc", description = "源IP地址",
+            required = true, multiValued = false)
+    String ipSrc = null;
+    @Argument(index = 0, name = "ipDst", description = "目的IP地址",
+            required = true, multiValued = false)
+    String ipDst = null;
+
+    @Override
+    protected void execute() {
+        ReactiveForwarding reactiveForwardingService = AbstractShellCommand.get(ReactiveForwarding.class);
+        MacAddress macAddress = null;
+        if (ipDst != null&&ipSrc!=null) {
+            System.out.println("========================   SearchPathByIpCommand   =============================");
+
+            System.out.println("=============================================================================");
+        }
+    }
+}
